84b370400adae36461cf916d371526c455a1105a
komstat <ykodama@nig.ac.jp>
2026-05-08 16:34:09 +0900

    seq validator changed

--- seq_validator.sh
+++ seq_validator.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# bsi-validator の DDBJ モードを呼び出す後方互換ラッパー
+# ddbj-validator の DDBJ モードを呼び出す後方互換ラッパー
 # readlink -f を使って、シンボリックリンク経由でも実体のディレクトリを正確に取得する
 SCRIPT_PATH=$(readlink -f "${BASH_SOURCE[0]}")
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 
 # 暗黙的に "ddbj" サブコマンドを差し込んで実行する
-"$SCRIPT_DIR/bsi-validator.sh" ddbj "$@"
+"$SCRIPT_DIR/ddbj-validator.sh" ddbj "$@"
