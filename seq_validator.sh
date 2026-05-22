a0eaf940acb3d845df540f6022c6f926631596f3
komstat <ykodama@nig.ac.jp>
2026-05-22 12:55:17 +0900

    deleted seq validator

--- seq_validator.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# ddbj-validator の DDBJ モードを呼び出す後方互換ラッパー
-# readlink -f を使って、シンボリックリンク経由でも実体のディレクトリを正確に取得する
-SCRIPT_PATH=$(readlink -f "${BASH_SOURCE[0]}")
-SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
-
-# 暗黙的に "ddbj" サブコマンドを差し込んで実行する
-"$SCRIPT_DIR/ddbj-validator.sh" ddbj "$@"
