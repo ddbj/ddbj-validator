1bae7ff94c303b73eda1d932f14c78dec8def84d
komstat <you1kodama@gmail.com>
2026-07-08 02:11:24 +0900

    docs: docker イメージ参照を latest に統一 (README + seq ラッパー)
    毎リリースでのバージョン書き換えを不要にするため、README.md の docker
    pull/run 例(日英 計6箇所)と、ラッパー ddbj-validator-seq.sh/.bat の
    イメージ参照を ghcr.io/ddbj/ddbj-validator:latest に変更。
    .bat は GHCR プレフィックスが欠けていたため補完(README/.sh と統一)。
    release.sh が毎リリースで latest タグも push するため常に最新版を指す。

--- ddbj-validator-seq.sh
+++ ddbj-validator-seq.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --rm -v "$(pwd):/data" -w /data ghcr.io/ddbj/ddbj-validator:0.1.0-beta "$@"
+docker run --rm -v "$(pwd):/data" -w /data ghcr.io/ddbj/ddbj-validator:latest "$@"
